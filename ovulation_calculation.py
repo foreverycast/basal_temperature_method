b58acf2b453377765c770e622579f1679696d4c9
foreverycast <47700055+foreverycast@users.noreply.github.com>
2021-01-06 21:09:02 +0100

    Created ovulation file calculation

---
+++ ovulation_calculation.py
@@ -0,0 +1,191 @@
+import datetime
+
+
+temperature_list = (
+    {'date': '2020-01-01', 'temperature': 36.7},
+    {'date': '2020-01-02', 'temperature': 36.6},
+    {'date': '2020-01-03', 'temperature': 36.6},
+    {'date': '2020-01-04', 'temperature': 36.6},
+    {'date': '2020-01-05', 'temperature': 36.6},
+    {'date': '2020-01-06', 'temperature': 36.6},
+    {'date': '2020-01-07', 'temperature': 36.6},
+    {'date': '2020-01-08', 'temperature': 36.5},
+    {'date': '2020-01-09', 'temperature': 36.5},
+    {'date': '2020-01-10', 'temperature': 36.5},
+    {'date': '2020-01-11', 'temperature': 36.5},
+    {'date': '2020-01-12', 'temperature': 36.5},
+    {'date': '2020-01-13', 'temperature': 36.5},
+    {'date': '2020-01-14', 'temperature': 36.6},
+    {'date': '2020-01-15', 'temperature': 36.6},
+    {'date': '2020-01-16', 'temperature': 36.8},
+    {'date': '2020-01-17', 'temperature': 36.8},
+    {'date': '2020-01-18', 'temperature': 36.8},
+    {'date': '2020-01-19', 'temperature': 36.8},
+    {'date': '2020-01-20', 'temperature': 36.9},
+    {'date': '2020-01-21', 'temperature': 36.9},
+    {'date': '2020-01-22', 'temperature': 36.9},
+    {'date': '2020-01-23', 'temperature': 36.9},
+    {'date': '2020-01-24', 'temperature': 36.9},
+    {'date': '2020-01-25', 'temperature': 37.0},
+    {'date': '2020-01-26', 'temperature': 36.9},
+    {'date': '2020-01-27', 'temperature': 36.8},
+    {'date': '2020-01-28', 'temperature': 36.6},
+)
+
+temperature_list = (
+    {'date': '2020-01-01', 'temperature': 36.6},
+    {'date': '2020-01-02', 'temperature': 36.7},
+    {'date': '2020-01-03', 'temperature': 36.5},
+    {'date': '2020-01-04', 'temperature': 36.6},
+    {'date': '2020-01-05', 'temperature': 37.4},
+    {'date': '2020-01-06', 'temperature': 37.3},
+    {'date': '2020-01-07', 'temperature': 36.8},
+    {'date': '2020-01-08', 'temperature': 36.8},
+    {'date': '2020-01-09', 'temperature': 36.9},
+    {'date': '2020-01-10', 'temperature': 36.7},
+    {'date': '2020-01-11', 'temperature': 36.8},
+    {'date': '2020-01-12', 'temperature': 36.6},
+    {'date': '2020-01-13', 'temperature': 36.6},
+    {'date': '2020-01-14', 'temperature': 36.6},
+    {'date': '2020-01-15', 'temperature': 36.5},
+    {'date': '2020-01-16', 'temperature': 36.4},
+    {'date': '2020-01-17', 'temperature': 36.7},
+    {'date': '2020-01-18', 'temperature': 36.9},
+    {'date': '2020-01-19', 'temperature': 37.1},
+    {'date': '2020-01-20', 'temperature': 37.2},
+    {'date': '2020-01-21', 'temperature': 37.2},
+    {'date': '2020-01-22', 'temperature': 37.3},
+    {'date': '2020-01-23', 'temperature': 37.0},
+    {'date': '2020-01-24', 'temperature': 37.0},
+    {'date': '2020-01-25', 'temperature': 37.1},
+    {'date': '2020-01-26', 'temperature': 36.9},
+    {'date': '2020-01-27', 'temperature': 37.0},
+    {'date': '2020-01-28', 'temperature': 37.0},
+    {'date': '2020-01-29', 'temperature': 36.9},
+    {'date': '2020-01-30', 'temperature': 36.8},
+)
+
+temperature_list_1 = (
+    {'temperature': 36.6, 'date': '2020-01-01'},
+    {'date': '2020-01-02', 'temperature' : 36.5},
+    {'date': '2020-01-03', 'temperature': 36.6},
+    {'date': '2020-01-04', 'temperature': 36.7},
+    {'date': '2020-01-05', 'temperature': 36.2},
+    {'date': '2020-01-06', 'temperature': 36.5},
+    {'date': '2020-01-07', 'temperature': 36.6},
+    {'date': '2020-01-08', 'temperature': 36.6},
+    {'date': '2020-01-09', 'temperature': 36.7},
+    {'date': '2020-01-10', 'temperature': 36.8},
+    {'date': '2020-01-11', 'temperature': 36.7},
+    {'date': '2020-01-12', 'temperature': 36.2},
+    {'date': '2020-01-13', 'temperature': 36.3},
+    {'date': '2020-01-14', 'temperature': 36.7},
+    {'date': '2020-01-15', 'temperature': 36.8},
+    {'date': '2020-01-16', 'temperature': 37.1},
+    {'date': '2020-01-17', 'temperature': 37.2},
+    {'date': '2020-01-18', 'temperature': 37.2},
+    {'date': '2020-01-19', 'temperature': 37.1},
+    {'date': '2020-01-20', 'temperature': 37.1},
+    {'date': '2020-01-21', 'temperature': 37.2},
+    {'date': '2020-01-22', 'temperature': 37.2},
+    {'date': '2020-01-23', 'temperature': 37.1},
+    {'date': '2020-01-24', 'temperature': 37.3},
+    {'date': '2020-01-25', 'temperature': 37.2},
+    {'date': '2020-01-26', 'temperature': 37.2},
+)
+
+temperature_list = (
+    {'date': '2020-01-01', 'temperature': 36.7},
+    {'date': '2020-01-02', 'temperature': 36.6},
+    {'date': '2020-01-03', 'temperature': 36.6},
+    {'date': '2020-01-04', 'temperature': 36.6},
+    {'date': '2020-01-05', 'temperature': 36.6},
+    {'date': '2020-01-06', 'temperature': 36.6},
+    {'date': '2020-01-07', 'temperature': 36.6},
+    {'date': '2020-01-08', 'temperature': 36.5},
+    {'date': '2020-01-09', 'temperature': 36.5},
+    {'date': '2020-01-10', 'temperature': 36.5},
+    {'date': '2020-01-11', 'temperature': 36.5},
+    {'date': '2020-01-12', 'temperature': 36.5},
+    {'date': '2020-01-13', 'temperature': 36.5},
+    {'date': '2020-01-14', 'temperature': 36.6},
+    {'date': '2020-01-15', 'temperature': 36.6},
+    {'date': '2020-01-16', 'temperature': 36.8},
+    {'date': '2020-01-17', 'temperature': 36.8},
+    {'date': '2020-01-18', 'temperature': 36.8},
+    {'date': '2020-01-19', 'temperature': 36.8},
+    {'date': '2020-01-20', 'temperature': 36.9},
+    {'date': '2020-01-21', 'temperature': 36.9},
+    {'date': '2020-01-22', 'temperature': 36.9},
+    {'date': '2020-01-23', 'temperature': 36.9},
+    {'date': '2020-01-24', 'temperature': 36.9},
+    {'date': '2020-01-25', 'temperature': 37.0},
+    {'date': '2020-01-26', 'temperature': 36.9},
+    {'date': '2020-01-27', 'temperature': 36.8},
+    {'date': '2020-01-28', 'temperature': 36.6},
+)
+
+def calculate_basal_method(temp_list):
+    """ Calculate ovaluation """
+    previous_date = datetime.datetime.now()
+    start_cycle_date = datetime.datetime.strptime(
+        temp_list[0]['date'], '%Y-%m-%d'
+    )
+
+    if len(temp_list) < 6:
+        return 'not_enough_measure_points'
+
+    temperature_previous = 0
+    counterRising = 0
+    for i in range(len(temp_list)):
+        date = datetime.datetime.strptime(
+            temp_list[i]['date'], '%Y-%m-%d'
+        )
+        
+        if temp_list[i]['temperature'] > 40:
+            return 'temperature_to_high'
+        
+        # 6 days search max
+        max_sichs = 0
+
+        # print((date - start_cycle_date).days)
+        temp_delta = temp_list[i]['temperature'] - temperature_previous
+        time_delta = (date - previous_date).days
+        
+        if time_delta > 4:
+            return 'delta_between_days_to_big'
+
+        if i > 2 and (date - start_cycle_date).days > 6 and \
+            temp_delta >= 0 and time_delta < 3:
+
+            for j in range(i):
+                # print(j, i)
+                search_max_date = datetime.datetime.strptime(
+                        temp_list[j]['date'], '%Y-%m-%d'
+                )
+                if (date - search_max_date).days < 7 and \
+                    max_sichs < temp_list[j]['temperature']:                
+                    max_sichs = temp_list[j]['temperature']
+
+            temp_delta_max = temp_list[i]['temperature'] - max_sichs
+    
+            print(max_sichs, temp_delta_max)
+
+            counterRising += 1
+            if counterRising > 2 and temp_delta_max >= 0.2:
+                print(datetime.datetime.strptime(
+                    temp_list[i]['date'], '%Y-%m-%d'
+                ), counterRising)
+                return date
+        else:
+            counterRising = 0
+        temperature_previous = temp_list[i]['temperature']
+        previous_date = datetime.datetime.strptime(
+            temp_list[i]['date'], '%Y-%m-%d'
+        )
+        
+
+
+print('start')
+print(calculate_basal_method(temperature_list))
+print('end')
